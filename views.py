5dfa1d6a9026c118ad9653b43e6dd69d2f24cc4c
Karolina Bagrowska <karolina.bagrowska@gmail.com>
2021-05-19 21:40:29 +0200

    Add HTMLResponse to get_cutsomer

--- views.py
+++ views.py
@@ -2,6 +2,7 @@ from starlette.responses import HTMLResponse
 from database import get_db
 from typing import List
 from fastapi import FastAPI, APIRouter, Depends
+from fastapi.responses import HTMLResponse
 from sqlalchemy.orm import Session
 import crud, models, schemas
 
@@ -13,3 +14,10 @@ async def get_customer(db: Session = Depends(get_db)):
 
     return crud.get_customers(db)
 
+@router.get("/customers_html")
+async def get_customer(db: Session = Depends(get_db)):
+    new_str = ""
+    for x in crud.get_customers(db):
+        content = f'<h1>Welcome {x.CompanyName} </h1>'
+        new_str += content
+    return HTMLResponse(content=new_str)
